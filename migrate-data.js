e714480ab568b802bedd9bb3c1b7c699f69a74e3
Alex Elliott <alex@canadianyankee.com>
2018-07-16 21:28:00 -0400

    Full implementation

--- migrate-data.js
+++ migrate-data.js
@@ -21,21 +21,35 @@ if(addressData.length !== totalRecords) {
 let numChunks = Math.ceil(totalRecords / chunkSize);
 console.log(`Will process ${numChunks} chunks of ${chunkSize} records each.`);
 
-let chunkStarts = Array(numChunks).fill().map((item, index) => index * chunkSize);
+// A factory function that manufactures taks to process a chunk of data
+function createTask(chunkIndex, db) {
+    let chunkStart = chunkIndex * chunkSize;
+    let data1 = Array.prototype.slice.call(customerData, chunkStart, chunkStart + chunkSize);
+    let data2 = Array.prototype.slice.call(addressData, chunkStart, chunkStart + chunkSize);
+    return (() => {
+        for(let i = 0; i < data1.length; i++) {
+            let data = data1[i];
+            Object.assign(data, data2[i]);
+            db.collection('customer-data').insert(data, (error, results, next) => {
+                if (error) return next(error);
+                console.log(`Finished records ${chunkStart} to ${chunkStart + data1.length}.`);
+            });
+        }
+    });
+}
 
 const url = 'mongodb://localhost:27017/edx-course-db';
 
 // Connect to the database
-//mongodb.connect(url, (error, client) => {
+mongodb.connect(url, (error, client) => {
     // Unlike what we were taught in the course, I have to dig one level into 
     // the object to find the thing we can use.
-//    let db = client.db('edx-course-db');
+    let db = client.db('edx-course-db');
 
-    async.parallel(chunkStarts, (chunkStart, callback) => {
-        console.log(`Processing records ${chunkStart} to ${chunkStart + chunkSize}`);
-        callback();
-    }, (error, results) => {
+    let tasks = Array(numChunks).fill().map((item, index) => createTask(index, db));
 
+    async.parallel(tasks, (error) => {
+        if(error) return console.log(error);
     });
-//});
+});
 
